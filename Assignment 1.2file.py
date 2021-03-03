e22d6229e5c9ccabedae2ce4c694b51b9af3ed58
dineshque <79956122+dineshque@users.noreply.github.com>
2021-03-03 09:35:42 +0530

    Assignment 1submitted

---
+++ Assignment 1.2file.py
@@ -0,0 +1,3 @@
+X=int(input('Enter the value of X:'))
+Y=int(input('Enter the value of Y:'))
+print(X**Y)
---
+++ assignment 1.1file.py
@@ -0,0 +1,25 @@
+#Python program to perform Addition, Subtraction,
+#Multiplication and division of two numbers
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+print("Enter the operator you want to perform");
+
+ch=input("Enter any of these operator for operation +, -, *, /  ")
+
+result=0
+if ch=='+':
+    result=num1+num2;
+elif ch=='-':
+    result=num1-num2;
+
+elif ch=='*':
+    result=num1*num2;
+elif ch=='/':
+    result=num1/num2;
+
+else:
+   print("char is not supported");
+
+print(num1,ch,num2,": ",result)
